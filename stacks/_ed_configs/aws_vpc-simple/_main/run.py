d1a9f78d7ee5f212fda7f2e45e847dc121c7458d
Gary Leong <gary@thytruth.com>
2023-02-16 17:39:31 +0800

    'change, upload, and test'

--- stacks/_ed_configs/aws_vpc-simple/_main/run.py
+++ stacks/_ed_configs/aws_vpc-simple/_main/run.py
@@ -90,7 +90,7 @@ def run(stackargs):
     stack.parse.add_optional(key="aws_default_region",default="us-east-1")
 
     # docker image to execute terraform with
-    stack.parse.add_optional(key="docker_exec_env",default="elasticdev/terraform-run-env:1.3.7")
+    stack.parse.add_optional(key="docker_exec_env",default="elasticdev/terraform-run-env:14")  # testtest 777 upgrade
 
     # if eks_cluster is specified, then add the correct tags to the vpc 
     stack.parse.add_optional(key="eks_cluster",default="null")
